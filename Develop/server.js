bbc6115aacf59eed850ca62abe5ed2859e16db6c
Austin Ellis <Austin@Janets-iMac.local>
2020-11-17 18:06:37 -0500

    begin call of all notes and sends to browser as an object array

--- Develop/server.js
+++ Develop/server.js
@@ -16,6 +16,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// routes
+
+// api call response for all the notes, and sends the results to the browser as an array of object
+
+app.get("/api/notes", function(err, res) {
+    try {
+      // reads the notes from json file
+      notesData = fs.readFileSync("db/db.json", "utf8");
+      console.log("Success!");
+      // parse it so notesData is an array of objects
+      notesData = JSON.parse(notesData);
+  
+      // error handling
+    } catch (err) {
+      console.log(err);
+    }
+    //   send objects to the browser
+    res.json(notesData);
+  });
+
+
 // Web page when the Get started button is clicked
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
