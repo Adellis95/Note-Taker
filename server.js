205e29622ce56f7646272339bab9839929aecbbe
Austin Ellis <Austin@Janets-iMac.local>
2020-11-18 22:56:09 -0500

    updated server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get("/notes", function(req, res) {
 });
 
 // If no matching route is found default to home
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
